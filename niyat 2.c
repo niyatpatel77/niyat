b32d7705e1f5ac4600fb44240e852d5054b1f48f
niyatpatel77 <niyatpatel77@gmail.com>
2026-08-21 10:17:54 +0530

    Add files via upload

---
+++ niyat 2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter the value of first number :");
+scanf("%d",&a);
+printf("\eter the value of second number:");
+scanf("%d",&b);
+if(a>b)
+printf("the higher valued number is %d,a");
+else
+printf("the higher valued number is %d,b");
+}
