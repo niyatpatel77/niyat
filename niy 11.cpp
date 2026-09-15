41e2324ede7a926b172cb70d26e78d0972a5f243
niyatpatel77 <niyatpatel77@gmail.com>
2026-09-15 21:07:36 +0530

    Add files via upload

---
+++ niy 11.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Loop runs 5 times, from i = 0 to i = 4
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "ChatGPT" << std::endl;
+    }
+    return 0;
+}
