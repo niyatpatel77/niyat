b58bb7e1c5416e4a0ebcf75785ca95b5ac59cbc7
niyatpatel77 <niyatpatel77@gmail.com>
2026-09-15 21:03:02 +0530

    Add files via upload

---
+++ niy 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Loop from 1 to 10 to print the first 10 natural numbers
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+    
+    cout << endl;
+    return 0;
+}
